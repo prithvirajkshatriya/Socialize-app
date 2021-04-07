564329470fc48515b8c6b8cf809a2ddf2981bdf8
Prithvi raj <1999prithviraj1999@gmail.com>
2021-04-07 23:36:10 +0530

    'Added support for restricted access to authencticated components'

--- client/auth/PrivateRoute.js
+++ client/auth/PrivateRoute.js
@@ -3,6 +3,10 @@ import React, { Component } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import auth from './auth-helper';
 
+// Components to be rendered into the PrivateRoute will load
+// only when the user's authenticated; determined by a call to
+// isAuthenticated() which otherwise, will be redirected to Signin component.
+
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
     {...rest}
@@ -11,10 +15,7 @@ const PrivateRoute = ({ component: Component, ...rest }) => (
         <Component {...props} />
       ) : (
         <Redirect
-          to={{
-            pathname: 'signin',
-            state: { from: props.location },
-          }}
+          to={{ pathname: '/signin', state: { from: props.location } }}
         />
       )
     }
